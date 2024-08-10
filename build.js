1dea68808a9f353aa3b71250a0ce24ed1c507957
Shafin <blackipie@outlook.com>
2024-08-10 13:15:45 +0530

    🐛 fixed

--- build.js
+++ build.js
@@ -1,20 +1,35 @@
 import esbuild from 'esbuild';
 import { dtsPlugin } from 'esbuild-plugin-d.ts';
 
-// Build configuration for esbuild
 esbuild.build({
   entryPoints: ['./src/index.ts'],
   bundle: true,
-  platform: 'node',
-  target: ['node14'],
-  outdir: 'dist',
+  platform: 'browser', 
+  target: ['es2020'],
+  outdir: 'dist/esm',
   sourcemap: false,
-  plugins: [dtsPlugin()],
-  minify: true,
+  plugins: [dtsPlugin({tsconfig:'tsconfig.json'})],
   tsconfig: './tsconfig.json',
   format: 'esm',
-  loader: {
-    '.ts': 'ts',
-    '.tsx': 'tsx'  // Handle JSX/TSX files
-  }
+  jsx:'automatic',
+  external: ['react', 'react-dom'],  
+  jsx: 'automatic',   
+  minify: true,
+}).catch(() => process.exit(1));
+
+
+esbuild.build({
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  platform: 'browser', 
+  target: ['es2020'],
+  outdir: 'dist/cjs',
+  sourcemap: false,
+  plugins: [dtsPlugin({tsconfig:'tsconfig.json'})],
+  tsconfig: './tsconfig.json',
+  format: 'cjs',
+  jsx:'automatic',
+  external: ['react', 'react-dom'], 
+  jsx: 'automatic',  
+  minify: true,
 }).catch(() => process.exit(1));
